a3cf6e4fbee27f86f1c94e1bf7e76ea7a5a75fb1
Frank Yu <kyu16@students.cs.ubc.ca>
2023-08-22 22:59:07 -0600

    Updated calculation system and implemented the decimal button

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -4,23 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+// CALCULATOR ROUNDS ALL DECIMALS TO ONE DECIMAL PLACE
 public class MainActivity extends AppCompatActivity {
+    private List<String> creatingNumbers = new ArrayList<>();
     private List<String> calculation = new ArrayList<>();
     private double currentNumber = 0;
     private double previousNumber = 0;
     private double rsf = 0;
-    private String currentOperation;
+    //private String currentOperation;
 
-    private TextView answer;
+    private EditText answer;
     private Button clearButton;
     private Button negativeButton;  //do last
     private Button percentageButton;  //do last
@@ -54,10 +56,11 @@ public class MainActivity extends AppCompatActivity {
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                creatingNumbers = new ArrayList<>();
                 calculation = new ArrayList<>();
                 previousNumber = 0;
                 currentNumber = 0;
-                currentOperation = "";
+                //currentOperation = "";
                 answer.setText("0");
             }
         });
@@ -72,43 +75,48 @@ public class MainActivity extends AppCompatActivity {
         displayingNumbers(seven, "7");
         displayingNumbers(eight, "8");
         displayingNumbers(nine, "9");
+        displayingNumbers(decimal, ".");
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentOperation = "+";
+                //currentOperation = "+";
                 operationClicked();
+                calculation.add("+");
             }
         });
 
         subtractButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentOperation = "-";
+                //currentOperation = "-";
                 operationClicked();
+                calculation.add("-");
             }
         });
 
         multiplyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentOperation = "*";
+                //currentOperation = "*";
                 operationClicked();
+                calculation.add("*");
             }
         });
 
         divisionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentOperation = "/";
+                //currentOperation = "/";
                 operationClicked();
+                calculation.add("/");
             }
         });
 
         equalsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (currentOperation == "+") { // ONLY FOR PLUS BUTTON RN
+/*                if (currentOperation == "+") { // ONLY FOR PLUS BUTTON RN
                     setCurrentNumber();
                     rsf = previousNumber + currentNumber;
                     printResult(rsf);
@@ -127,6 +135,25 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     answer.setText("Error");
                 }
+                currentOperation = "=";
+*/
+                setCurrentNumber();
+
+                rsf = Double.parseDouble(calculation.get(0));
+
+                for (int i = 1; i < calculation.size(); i++) {
+                    if (calculation.get(i) == "+") {
+                        rsf += Double.parseDouble(calculation.get(i+1));
+                    } else if (calculation.get(i) == "-") {
+                        rsf -= Double.parseDouble(calculation.get(i+1));
+                    } else if (calculation.get(i) == "*") {
+                        rsf *= Double.parseDouble(calculation.get(i+1));
+                    } else if (calculation.get(i) == "/") {
+                        rsf /= Double.parseDouble(calculation.get(i+1));
+                    }
+                }
+
+                printResult(rsf);
             }
         });
     }
@@ -136,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calculation.add(num);
+                creatingNumbers.add(num);
                 String result = getNumber();
                 //result = creatingNumbers(num);
                 answer.setText(result);
@@ -147,40 +174,54 @@ public class MainActivity extends AppCompatActivity {
     //sets currentNumber when equal sign is clicked
     private void setCurrentNumber() {
         String result = getNumber();
-        currentNumber = Double.parseDouble(result);
+        //currentNumber = Double.parseDouble(result);
+        creatingNumbers.clear();
+        calculation.add(result);
     }
 
     //sets previousNumber when an operation is clicked
     private void operationClicked() {
         String result = getNumber();
-        previousNumber = Double.parseDouble(result);
+        //previousNumber = Double.parseDouble(result);
 
-        calculation.clear();
+        creatingNumbers.clear();
+        calculation.add(result);
         answer.setText("0");
-    }
-
-    //prints the result after the equals button is pressed
-    private void printResult(double r) {
-        calculation.clear();
-        calculation.add(Double.toString(r));
-
-        DecimalFormat format = new DecimalFormat("0.#");
-        answer.setText(format.format(r));
     }
 
     // brings the numbers inputted together into one number
     @NonNull
     private String getNumber() {
         String result = "";
-        for (int i = 0; i < calculation.size(); i++) {
-            result += calculation.get(i);
+        for (int i = 0; i < creatingNumbers.size(); i++) {
+            result += creatingNumbers.get(i);
         }
+
+        if (result.indexOf(".") != result.lastIndexOf(".")) {
+            creatingNumbers = new ArrayList<>();
+            calculation = new ArrayList<>();
+            previousNumber = 0;
+            currentNumber = 0;
+            answer.setText("Error");
+            return "Error";
+        }
+
         return result;
     }
 
+    //prints the result after the equals button is pressed
+    private void printResult(double r) {
+        creatingNumbers.clear();
+        creatingNumbers.add(Double.toString(r));
+
+        DecimalFormat format = new DecimalFormat("0.########");
+        answer.setText(format.format(r));
+    }
+
+
     //assign all the buttons/textview to the private variables above
     private void setUp() {
-        answer = (TextView) findViewById(R.id.answer);
+        answer = (EditText) findViewById(R.id.answer);
 
         clearButton = (Button) findViewById(R.id.clearbutton);
         negativeButton = (Button) findViewById(R.id.negativesignbutton);
